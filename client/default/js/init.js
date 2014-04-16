b845a8aa592913826f61e97c40a3573c87058604
Brian Dooley <brian.dooley@feedhenry.com>
2014-04-16 17:05:55 +0100

    initial adjustment

--- client/default/js/init.js
+++ client/default/js/init.js
@@ -7,24 +7,16 @@ call which will return the remote config.
 */
 
 $fh.ready(function() {
-  document.getElementById('run_button').onclick = function() {
+  document.getElementById('submit_button').onclick = function() {
 
-  console.log(document.getElementById("sqlquery").value);
-
-  $fh.act(
-    {
-      act:'mysql',
-      req: {
-                'query': document.getElementById("sqlquery").value
-           }
-    },
-    function(res) {
-      document.getElementById('sqlResults').innerHTML = JSON.stringify(res.rows);
-    },
-    function(code,errorprops,params) {
-      console.log('An error occured: ' + code + ' : ' + errorprops.error);
-      document.getElementById('sqlResults').innerHTML = 'An error occured: ' + code + ' : ' + errorprops.error;
-    }
-  );
+    $fh.send({
+    to: 'brian.dooley@gmail.com',
+    subject: 'Hi There',
+    body: 'This is a message.'
+  }, function() {
+    alert("message sent.")
+  }, function(msg, err) {
+    alert("Send failed. Error is " + msg)
+  })
 };
 });
