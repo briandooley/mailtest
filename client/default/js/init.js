1485a3ae4c040a0d184f358dc84c81bd37de9d41
Brian Dooley <brian.dooley@feedhenry.com>
2014-04-16 17:18:42 +0100

    initial alert

--- client/default/js/init.js
+++ client/default/js/init.js
@@ -9,16 +9,16 @@ call which will return the remote config.
 $fh.ready(function() {
   document.getElementById('submit_button').onclick = function() {
 
-    alert("About to send.")
+    alert("About to send.");
 
     $fh.send({
     to: 'brian.dooley@gmail.com',
     subject: 'Hi There',
     body: 'This is a message.'
   }, function() {
-    alert("message sent.")
+    alert("message sent.");
   }, function(msg, err) {
-    alert("Send failed. Error is " + msg)
+    alert("Send failed. Error is " + msg);
   })
 };
 });
