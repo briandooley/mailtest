acdc62a9a55134fef1a4b9fdbcb70272498c1dc1
brian.dooley@feedhenry.com <brian.dooley@feedhenry.com>
2014-05-22 11:34:39 +0000

    update file /client/default/js/init.js

--- client/default/js/init.js
+++ client/default/js/init.js
@@ -9,6 +9,19 @@ call which will return the remote config.
 $fh.ready(function() {
   document.getElementById('submit_button').onclick = function() {
 
+  alert('Saving...');
+  
+  $fh.data({
+      act: "save",
+      key: "draft_21756_INS20140521144704",
+      val: '{"id":"INS20140521144704","validate":false,"uuid":"F63E6923D8FF4EAB8302AB3FF48A9AD1","username":"m1crawley","userid":"21756","creationdate":"2014-05-21T13:47:04.126","completedate":"2014-05-21T13:47:04.126","DivisionId":0,"inspectionCategory":"1","inspectionType":0,"inspectionSource":0,"inspectionContract":"","gang":0,"people":[],"localArea":0,"Location":{"Label":"","Latitude":0,"Longitude":0,"MapVisible":true,"LatLongSet":false,"OSGridReference":"","HouseNumber":"","Road":"","Town":"","County":"","Country":null,"PostCode":""},"job":{"ordernumber":"","clientreference":"","noticereference":"","vehiclereference":"","worksdescription":"","highwayauthority":"","reinstatement":null,"jobtype":"","supervisor":""},"checkSheets":[]}'
+    }, function() {
+      alert('Success');
+    }, function(msg, err) {
+      alert('Fail: ' + JSON.stringify(err) + "/" + msg);
+    });
+
+/*
     alert("About to send.");
 
     $fh.send({
@@ -21,5 +34,7 @@ $fh.ready(function() {
   }, function(msg, err) {
     alert("Send failed. Error is " + msg);
   });
+*/
+
 };
 });
